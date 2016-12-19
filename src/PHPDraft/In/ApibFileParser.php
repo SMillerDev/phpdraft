4259a2a5e623e341eb359c4bde9a3a7e9c52100c
Sean Molenaar <sean@m2mobi.com>
2016-12-19 12:18:14 +0100

    Pull external schemas and fix example URL

--- src/PHPDraft/In/ApibFileParser.php
+++ src/PHPDraft/In/ApibFileParser.php
@@ -60,6 +60,11 @@ class ApibFileParser
                 $this->get_apib($this->location . $value . '.apib'), $file);
         }
 
+        preg_match_all('<!-- schema\(([a-z0-9_.\/\:]*?)\) -->', $file, $matches);
+        foreach ($matches[1] as $value) {
+            $file = str_replace('<!-- schema(' . $value . ') -->', $this->get_schema($value), $file);
+        }
+
         return $file;
     }
 
@@ -72,11 +77,29 @@ class ApibFileParser
      */
     private function file_check($filename)
     {
-        if (!file_exists($filename))
-        {
+        if (!file_exists($filename)) {
             file_put_contents('php://stderr', "API File not found: $filename\n");
             exit(1);
         }
+    }
+
+    /**
+     * Get an external Schema by URL
+     *
+     * @param string $url URL to fetch the schema from
+     *
+     * @return string The schema as a string
+     */
+    function get_schema($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return $result;
     }
 
     /**
